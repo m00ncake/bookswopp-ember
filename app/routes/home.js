a8877fcab97b5514ebcee559409e76aa5dd97847
John Moon <johntylermoon@gmail.com>
2017-09-26 14:02:09 -0700

    model update

--- app/routes/home.js
+++ app/routes/home.js
@@ -3,31 +3,73 @@ import Ember from 'ember';
 export default Ember.Route.extend({
     model() {
         let book01 = {
+            isPopular: false,
             image: './assets/images/book-1.png',
             title: 'The Wonderful Things You Will Be',
             author: 'Emily Winfield Martin'
         };
         let book02 = {
+            isPopular: false,            
             image: './assets/images/book-2.png',
             title: 'The Green Tree',
             author: 'Shelly Silverton'
         };
         let book03 = {
+            isPopular: false,            
             image: './assets/images/book-3.png',
             title: 'Dragon Loves Tacos',
             author: 'Adam Rubin'
         };
         let book04 = {
+            isPopular: false,            
             image: './assets/images/book-4.png',
             title: 'If Animals Kissed Good Night',
             author: 'Kate Bloomfield'
         };
         let book05 = {
+            isPopular: false,            
             image: './assets/images/book-5.png',
             title: 'The Monsters At the End of This Book',
             author: 'Grover'
         };
         let book06 = {
+            isPopular: false,          
+            image: './assets/images/book-6.png',
+            title: 'The Rainbow Fish',
+            author: 'Marcus Penter'
+        };
+        let book07 = {
+            isPopular: true,          
+            image: './assets/images/book-6.png',
+            title: 'The Rainbow Fish',
+            author: 'Marcus Penter'
+        };
+        let book08 = {
+            isPopular: true,          
+            image: './assets/images/book-6.png',
+            title: 'The Rainbow Fish',
+            author: 'Marcus Penter'
+        };
+        let book09 = {
+            isPopular: true,          
+            image: './assets/images/book-6.png',
+            title: 'The Rainbow Fish',
+            author: 'Marcus Penter'
+        };
+        let book10 = {
+            isPopular: true,          
+            image: './assets/images/book-6.png',
+            title: 'The Rainbow Fish',
+            author: 'Marcus Penter'
+        };
+        let book11 = {
+            isPopular: true,          
+            image: './assets/images/book-6.png',
+            title: 'The Rainbow Fish',
+            author: 'Marcus Penter'
+        };
+        let book12 = {
+            isPopular: true,          
             image: './assets/images/book-6.png',
             title: 'The Rainbow Fish',
             author: 'Marcus Penter'
