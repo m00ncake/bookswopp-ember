18714f8269252686e815ef2cdf88a470d98dd2f9
John Moon <johntylermoon@gmail.com>
2017-10-03 11:27:25 -0700

    mirage installed

--- app/router.js
+++ app/router.js
@@ -7,7 +7,6 @@ const Router = Ember.Router.extend({
 });
 
 Router.map(function() {
-  this.route('books');
   this.route('home', { path: '/' });
 });
 
---
+++ mirage/fixtures/books.js
@@ -0,0 +1,86 @@
+export default [
+    {
+        id: 1,
+        isPopular: false,
+        image: './assets/images/book-1.png',
+        title: 'The Wonderful Things You Will Be',
+        author: 'Emily Winfield Martin'
+    },
+    {
+        id: 2,
+        isPopular: false,            
+        image: './assets/images/book-2.png',
+        title: 'The Green Tree',
+        author: 'Shelly Silverton'
+    },
+    {
+        id: 3,
+        isPopular: false,            
+        image: './assets/images/book-3.png',
+        title: 'Dragon Loves Tacos',
+        author: 'Adam Rubin'
+    },
+    {
+        id: 4,
+        isPopular: false,            
+        image: './assets/images/book-4.png',
+        title: 'If Animals Kissed Good Night',
+        author: 'Kate Bloomfield'
+    },
+    {
+        id: 5,
+        isPopular: false,            
+        image: './assets/images/book-5.png',
+        title: 'The Monsters At the End of This Book',
+        author: 'Grover'
+    },
+    {
+        id: 6,
+        isPopular: false,          
+        image: './assets/images/book-6.png',
+        title: 'The Rainbow Fish',
+        author: 'Marcus Penter'
+    },
+    {
+        id: 7,
+        isPopular: true,          
+        image: './assets/images/book-7.png',
+        title: 'If You Give a Mouse a Cookie',
+        author: 'Numeroff'
+    },
+    {
+        id: 8,
+        isPopular: true,          
+        image: './assets/images/book-8.png',
+        title: 'The Story of Nerdiland',
+        author: 'Munro Leaf'
+    },
+    {
+        id: 9,
+        isPopular: true,          
+        image: './assets/images/book-9.png',
+        title: 'The Rainbow Fish',
+        author: 'Marcus Penter'
+    },
+    {
+        id: 10,
+        isPopular: true,          
+        image: './assets/images/book-10.png',
+        title: 'The Rainbow Fish',
+        author: 'Marcus Penter'
+    },
+    {
+        id: 11,
+        isPopular: true,          
+        image: './assets/images/book-11.png',
+        title: 'The Rainbow Fish',
+        author: 'Marcus Penter'
+    },
+    {
+        id: 12,
+        isPopular: true,          
+        image: './assets/images/book-12.png',
+        title: 'The Rainbow Fish',
+        author: 'Marcus Penter'
+    }
+];
