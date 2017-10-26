6445b58a0a1d0a7a8f29c7182a4089c552c583f8
John Moon <johntylermoon@gmail.com>
2017-10-25 17:07:56 -0700

    Login in Page Created

--- app/controllers/home.js
+++ app/controllers/home.js
@@ -6,6 +6,9 @@ export default Ember.Controller.extend({
     actions: {
         gotoSignUp() {
             this.transitionToRoute('sign-up');
+        },
+        select() {
+            console.log('hello');
         }
     }
 });
--- app/router.js
+++ app/router.js
@@ -9,6 +9,7 @@ const Router = Ember.Router.extend({
 Router.map(function() {
   this.route('home', { path: '/' });
   this.route('sign-up');
+  this.route('login');
 });
 
 export default Router;
